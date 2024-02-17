4de40647f251b428f9485b5b368dcbdfe8389b05
sanket <sanketshinde3123@gmail.com>
2024-02-18 00:37:38 +0530

    one11

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,11 @@ import Mp_section6 from './Components/Mp_section6'
 import Mp_contatctus from './Components/Mp_contatctus'
 import Mp_footer from './Components/Mp_footer'
 import Mp_slider from './Components/Mp_slider'
-import Mp_Navbar from './Components/Mp_Navbar'
 
 const page = () => {
 
   return ( 
     <div>
-    <Mp_Navbar/>
     <Mp_hero/>
     <Mp_stats/>
     <Mp_section1/>
